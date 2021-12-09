f9455317312bacdeb01494d3f0c1a928ad16ce9f
unknown <ptvce@yahoo.com>
2021-12-09 11:56:56 +1100

    init a windows app to calculate your profit

---
+++ InventoryProfit/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace InventoryProfit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInventory = new System.Windows.Forms.TextBox();
+            this.txtPercent = new System.Windows.Forms.TextBox();
+            this.txtDays = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDays = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblLastInventory = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLastInventory = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.lblProfit = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblGoal = new System.Windows.Forms.Label();
+            this.lblDescription2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInventory
+            // 
+            this.txtInventory.Location = new System.Drawing.Point(536, 42);
+            this.txtInventory.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtInventory.Name = "txtInventory";
+            this.txtInventory.Size = new System.Drawing.Size(251, 32);
+            this.txtInventory.TabIndex = 0;
+            this.txtInventory.TextChanged += new System.EventHandler(this.txtInventory_TextChanged);
+            // 
+            // txtPercent
+            // 
+            this.txtPercent.Location = new System.Drawing.Point(536, 84);
+            this.txtPercent.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtPercent.Name = "txtPercent";
+            this.txtPercent.Size = new System.Drawing.Size(251, 32);
+            this.txtPercent.TabIndex = 1;
+            // 
+            // txtDays
+            // 
+            this.txtDays.Location = new System.Drawing.Point(536, 128);
+            this.txtDays.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(249, 32);
+            this.txtDays.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(795, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "سرمایه:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(794, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(103, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "درصد سود:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(792, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(39, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "روز:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDays);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lblLastInventory);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(410, 324);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(534, 216);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result/نتیجه:";
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblDays.Location = new System.Drawing.Point(107, 100);
+            this.lblDays.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(0, 24);
+            this.lblDays.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(393, 100);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 24);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Day/روز:";
+            // 
+            // lblLastInventory
+            // 
+            this.lblLastInventory.AutoSize = true;
+            this.lblLastInventory.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblLastInventory.Location = new System.Drawing.Point(91, 57);
+            this.lblLastInventory.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblLastInventory.MaximumSize = new System.Drawing.Size(275, 300);
+            this.lblLastInventory.Name = "lblLastInventory";
+            this.lblLastInventory.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblLastInventory.Size = new System.Drawing.Size(0, 24);
+            this.lblLastInventory.TabIndex = 1;
+            this.lblLastInventory.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(393, 45);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Goal/هدف:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Image = global::InventoryProfit.Properties.Resources._1371475973_document_edit;
+            this.btnCalculate.Location = new System.Drawing.Point(538, 228);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(249, 58);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate/محاسبه";
+            this.btnCalculate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(504, 87);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "%";
+            // 
+            // txtLastInventory
+            // 
+            this.txtLastInventory.Location = new System.Drawing.Point(536, 170);
+            this.txtLastInventory.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtLastInventory.Name = "txtLastInventory";
+            this.txtLastInventory.Size = new System.Drawing.Size(249, 32);
+            this.txtLastInventory.TabIndex = 9;
+            this.txtLastInventory.TextChanged += new System.EventHandler(this.txtLastInventory_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(794, 174);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(58, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "هدف:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InventoryProfit.Properties.Resources.Untitled_1;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 244);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(357, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label8.Location = new System.Drawing.Point(25, 526);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 14);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "www.vanda.ir";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblDescription.ForeColor = System.Drawing.Color.Red;
+            this.lblDescription.Location = new System.Drawing.Point(25, 45);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(430, 18);
+            this.lblDescription.TabIndex = 13;
+            this.lblDescription.Text = "سرمایه، درصد سود و یکی از فیلد های روز یا هدف حتما باید پر شود.";
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblBalance.Location = new System.Drawing.Point(898, 43);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(46, 21);
+            this.lblBalance.TabIndex = 14;
+            this.lblBalance.Text = "Fund";
+            // 
+            // lblProfit
+            // 
+            this.lblProfit.AutoSize = true;
+            this.lblProfit.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblProfit.Location = new System.Drawing.Point(898, 95);
+            this.lblProfit.Name = "lblProfit";
+            this.lblProfit.Size = new System.Drawing.Size(66, 21);
+            this.lblProfit.TabIndex = 15;
+            this.lblProfit.Text = "%Profit";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblDay.Location = new System.Drawing.Point(898, 134);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(39, 21);
+            this.lblDay.TabIndex = 16;
+            this.lblDay.Text = "Day";
+            // 
+            // lblGoal
+            // 
+            this.lblGoal.AutoSize = true;
+            this.lblGoal.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblGoal.Location = new System.Drawing.Point(898, 170);
+            this.lblGoal.Name = "lblGoal";
+            this.lblGoal.Size = new System.Drawing.Size(43, 21);
+            this.lblGoal.TabIndex = 17;
+            this.lblGoal.Text = "Goal";
+            // 
+            // lblDescription2
+            // 
+            this.lblDescription2.AutoSize = true;
+            this.lblDescription2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblDescription2.ForeColor = System.Drawing.Color.Red;
+            this.lblDescription2.Location = new System.Drawing.Point(74, 92);
+            this.lblDescription2.Name = "lblDescription2";
+            this.lblDescription2.Size = new System.Drawing.Size(311, 18);
+            this.lblDescription2.TabIndex = 18;
+            this.lblDescription2.Text = "Fund, Profit, Day or Goal are mandatories field";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(977, 560);
+            this.Controls.Add(this.lblDescription2);
+            this.Controls.Add(this.lblGoal);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblProfit);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtLastInventory);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.txtPercent);
+            this.Controls.Add(this.txtInventory);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "هدف گذاری";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInventory;
+        private System.Windows.Forms.TextBox txtPercent;
+        private System.Windows.Forms.TextBox txtDays;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblLastInventory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtLastInventory;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.Label lblProfit;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label lblGoal;
+        private System.Windows.Forms.Label lblDescription2;
+    }
+}
+
---
+++ InventoryProfit/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryProfit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            lblDays.Text = "";
+            lblLastInventory.Text = "";
+
+            var money = Convert.ToDouble(txtInventory.Text);
+
+            if (txtLastInventory.Text == null || txtLastInventory.Text == "")
+            {
+                if (txtDays.Text != "" && txtDays.Text != null)
+                {
+                    int days = Convert.ToInt16(txtDays.Text);
+                    for (int i = 0; i < days; i++)
+                    {
+                        var percent = money * Convert.ToDouble(Convert.ToDecimal(txtPercent.Text) / 100);
+                        money = percent + money;
+                    }
+
+                    lblLastInventory.Text = Convert.ToDouble(money).ToString("#,##0.00");
+                }
+            }
+            else if (txtDays.Text == null || txtDays.Text == "")
+            {
+                if (txtLastInventory.Text != null && txtLastInventory.Text != "")
+                {
+                    int days = 0;
+                    string lastInventory = txtLastInventory.Text.Replace(",", "");
+                    while (money < Convert.ToInt64(lastInventory))
+                    {
+                        var percent = money * Convert.ToDouble(Convert.ToDecimal(txtPercent.Text) / 100);
+                        money = percent + money;
+                        days++;
+                    }
+                    lblDays.Text = days.ToString("#,##0.00");
+                }
+            }
+            //else if (txtPercent.Text == null || txtPercent.Text == "")
+            //{
+            //    int days = Convert.ToInt16(txtDays.Text);
+            //    while (money < Convert.ToInt64(txtLastInventory.Text))
+            //    {
+            //        for (int i = 0; i < days; i++)
+            //        {
+            //            var percent = money * Convert.ToDecimal(Convert.ToDecimal(txtPercent.Text) / 100);
+            //            money = percent + money;
+            //        }
+            //    }
+            //}
+        }
+
+        public string setComma(double number)
+        {
+            string x = string.Format("{0:n0}", number);
+            return x;
+        }
+        public double UptoTwoDecimalPoints(double num)
+        {
+            var totalCost = Convert.ToDouble(String.Format("{0:0.00}", num));
+            return totalCost;
+        }
+
+        private void txtInventory_TextChanged(object sender, EventArgs e)
+        {
+            string value = txtInventory.Text.Replace(",", "");
+            ulong ul;
+            if (ulong.TryParse(value, out ul))
+            {
+                txtInventory.TextChanged -= txtInventory_TextChanged;
+                txtInventory.Text = string.Format("{0:#,#}", ul);
+                txtInventory.SelectionStart = txtInventory.Text.Length;
+                txtInventory.TextChanged += txtInventory_TextChanged;
+            }
+        }
+
+        private void txtLastInventory_TextChanged(object sender, EventArgs e)
+        {
+            string value = txtLastInventory.Text.Replace(",", "");
+            ulong ul;
+            if (ulong.TryParse(value, out ul))
+            {
+                txtLastInventory.TextChanged -= txtLastInventory_TextChanged;
+                txtLastInventory.Text = string.Format("{0:#,#}", ul);
+                txtLastInventory.SelectionStart = txtLastInventory.Text.Length;
+                txtLastInventory.TextChanged += txtLastInventory_TextChanged;
+            }
+        }
+    }
+}
